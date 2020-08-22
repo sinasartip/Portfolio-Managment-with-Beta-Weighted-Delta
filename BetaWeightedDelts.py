c5a0c3e9c58ce3b333a8dabe0498fcc859ffb0e7
sinasartip <ssartipz@ryerson.ca>
2020-08-22 00:54:40 -0400

    made into functions

--- BetaWeightedDelts.py
+++ BetaWeightedDelts.py
@@ -6,54 +6,80 @@ from scipy.stats import linregress
 
 #%matplotlib inline
 
-# %%
-#Read in portfolio values & calculate the percent change 
-#Reference is the reference value
+def BetaWeightedDelta(StockName, ReferenceName, plot = True): 
+    """The beta weighted delta shows how many points your stock will move
+    for any given move of the reference. 
+    The beta weighting allows you to add the various deltas of your portfolio to 
+    find a final whole portfolio delta, normalized to the reference. 
+    
+    Inputs : 
+        StockName = Directory to you Stock (use the stock ticker as file name)
+        ReferenceName = Directory to you Stock (use the stock ticker as file name)
+        
+        Use yahoo finance histric prices and place them all in the same folder. 
+        The number of data points can be different, the function will adjust the sizes. 
+        
+        plot : If you want to see a plot of the percent price changes and the beta regression line 
+               you also get the R^2 value for a wellness of fit. 
+               
+    Output : 
+        Beta : beta weighting of your stocks  delta 
+        R^2 : wellness of fit of the beta  """
+    #Read in portfolio values & calculate the percent change 
+    #Reference is the reference value
 
-StockName = './correlation/XIU.TO.csv'
-ReferenceName = './correlation/XIC.TO.csv'
 
-Stock = pd.read_csv(StockName)
-Stock['pct'] = Stock['Close'].pct_change()
-Reference = pd.read_csv(ReferenceName)
-Reference['pct'] = Reference['Close'].pct_change()
+    Stock = pd.read_csv(StockName)
+    Stock['pct'] = Stock['Close'].pct_change()
+    Reference = pd.read_csv(ReferenceName)
+    Reference['pct'] = Reference['Close'].pct_change()
 
-# %%
-#The first element is NaN
-Stock_pct = Stock['pct'].to_numpy()
-Stock_pct = np.delete(Stock_pct, 0)
-Reference_pct = Reference['pct'].to_numpy()
-Reference_pct = np.delete(Reference_pct, 0)
-print(f"Shape of reference: {Reference_pct.shape}")
-print(f"Shape of Stock {Stock_pct.shape}")
+ 
+    #The first element is NaN
+    Stock_pct = Stock['pct'].to_numpy()
+    Stock_pct = np.delete(Stock_pct, 0)
+    Reference_pct = Reference['pct'].to_numpy()
+    Reference_pct = np.delete(Reference_pct, 0)
+    #print(f"Shape of reference: {Reference_pct.shape}")
+    #print(f"Shape of Stock {Stock_pct.shape}")
 
-if Reference_pct.shape != Stock_pct.shape:
-    ShapeDiff = abs(Reference_pct.shape[0] - Stock_pct.shape[0])
-    print(f"Shape Difference {ShapeDiff}")
-    if Reference_pct.shape < Stock_pct.shape:
-        Stock_pct = Stock_pct[:-ShapeDiff]
-    else:
-        Reference_pct = Reference_pct[:-ShapeDiff]
-    print("After Adjustment")
-    print(Reference_pct.shape)
-    print(Stock_pct.shape)
+    #adjust shape of input data if needed 
+    if Reference_pct.shape != Stock_pct.shape:
+        ShapeDiff = abs(Reference_pct.shape[0] - Stock_pct.shape[0])
+        print(f"Shape Difference {ShapeDiff}")
+        if Reference_pct.shape < Stock_pct.shape:
+            Stock_pct = Stock_pct[:-ShapeDiff]
+        else:
+            Reference_pct = Reference_pct[:-ShapeDiff]
+        print("After Adjustment")
+        print(Reference_pct.shape)
+        print(Stock_pct.shape)
 
-#%% 
+    #find beta
+    slope, intercept, r_value, p_value, std_err = linregress(Reference_pct, Stock_pct)
+    print(f"beta = {slope:.3}")
+    print(f"R^2 = {r_value:.3}")
+    label = r"$R^{2}$" + f" {r_value:.3}"
+    beta = slope
 
-slope, intercept, r_value, p_value, std_err = linregress(Reference_pct, Stock_pct)
-print(f"beta = {slope:.3}")
-print(f"R^2 = {r_value:.3}")
-label = r"$R^{2}$" + f" {r_value:.3}"
-beta = slope
-x = np.linspace(Reference_pct.min(),Reference_pct.max())
-y = slope * x + intercept
+    #plot if requested
+    if plot == True:    
+        x = np.linspace(Reference_pct.min(),Reference_pct.max())
+        y = slope * x + intercept
 
-fig, axs = plt.subplots()
-axs.plot(Reference_pct, Stock_pct, '.')
-axs.plot(x,y,'k',label = label)
-axs.grid(True)
-axs.set_xlabel('dS/S')
-axs.set_ylabel('dR/R')
-axs.set_title(f"Beta of {StockName[14:-4]} to {ReferenceName[14:-4]}")
-axs.legend()
-# %%
+        fig, axs = plt.subplots()
+        axs.plot(Reference_pct, Stock_pct, '.')
+        axs.plot(x,y,'k',label = label)
+        axs.grid(True)
+        axs.set_xlabel('dS/S')
+        axs.set_ylabel('dR/R')
+        axs.set_title(f"Beta of {StockName[14:-4]} to {ReferenceName[14:-4]}")
+        axs.legend()
+        plt.show()
+    return([beta, r_value]) 
+
+if __name__ == "__main__":
+    stock = './correlation/XIU.TO.csv'
+    reference = './correlation/XIC.TO.csv'
+
+    BetaWeightedDelta(stock, reference, False)
--- StockCorrMatrix.py
+++ StockCorrMatrix.py
@@ -1,4 +1,3 @@
-#%%
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import numpy as np
@@ -21,32 +20,40 @@ def dfsize(dir):
     
     return sizes.max(axis=0)
 
-#%%
-#create an array of stock names and stock pct
-dir = "./correlation"
-dir = sorted_alphanumeric(os.listdir(dir))
+def StockCorrelationMatrix(dir, plot = True):
+    #create an array of stock names and stock pct
+    dir = sorted_alphanumeric(os.listdir(dir))
 
-data = []
-headers = []
+    data = []
+    headers = []
+    try:    
+        for index,file in enumerate(dir):
+            headers.append(file[:-4])
+            Stock = pd.read_csv(f"./correlation/{file}")
+            Stock['pct'] = Stock['Close'].pct_change()
+            data.append(Stock['pct'])
+        pct_frame = pd.concat(data, axis=1, keys = headers)
+        pct_frame = pct_frame.iloc[1:,] #delete the first the row (NaN)
+            
 
-for index,file in enumerate(dir):
-    headers.append(file[:-4])
-    Stock = pd.read_csv(f"./correlation/{file}")
-    Stock['pct'] = Stock['Close'].pct_change()
-    data.append(Stock['pct'])
-pct_frame = pd.concat(data, axis=1, keys = headers)
-pct_frame = pct_frame.iloc[1:,] #delete the first the row (NaN)
-    
-# %%
-#heat map and correlation
+        #heat map and correlation
 
-corr_frame = pct_frame.corr()
+        corr_frame = pct_frame.corr()
+        if plot == True:
+            #setup figure
+            fig, ax = plt.subplots()
+            
+            # Create a mask to hide upper part of the correlation matrix
+            mask = np.triu(np.ones_like(corr_frame, dtype=np.bool))
+            seaborn.heatmap(corr_frame, cmap='YlGnBu', mask=mask)
+            plt.show()
+    except Exception as error:
+        print(error)
+        print("There is likely a non csv file type in the folder")
 
-#setup figure
-fig, ax = plt.subplots()
-
-#  Create a mask to hide upper part of the correlation matrix
-mask = np.triu(np.ones_like(corr_frame, dtype=np.bool))
-seaborn.heatmap(corr_frame, cmap='YlGnBu', mask=mask)
-plt.show()
-# %%
+if __name__ == "__main__":
+   dir = "./correlation"
+   StockCorrelationMatrix(dir)
+   #You need to output array of stock with correlation above 0.6, there will be sets of stocks with this 
+   #setup you should choose the largest
+   
