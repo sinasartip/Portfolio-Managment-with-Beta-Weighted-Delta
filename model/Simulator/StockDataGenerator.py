31de0fa3643ded21a4c721d4e6ee30c09e95e7e0
sinasartip <ssartipz@ryerson.ca>
2021-01-15 22:38:09 -0500

    price generator done

--- model/Simulator/StockDataGenerator.py
+++ model/Simulator/StockDataGenerator.py
@@ -1,5 +1,17 @@
+from math import sqrt, exp
+from random import gauss, seed
 
 
-class PriceGenerator():
-    def __init__(self):
-        pass
+class GBM_PriceGenerator():
+    def __init__(self, initial_price, annual_interest, volatility):
+        self.initial_price = initial_price
+        self.interest_rate = annual_interest
+        self.volatility = volatility
+
+    def generate_value(self):
+        current_price = self.initial_price
+        mu = self.interest_rate
+        sigma = self.volatility
+
+        current_price *= exp((mu - 0.5 * sigma**2) * (1.0/365.0) * gauss(mu = 0, sigma = 1))
+        return current_price
