d262baf2c599ab8e36f64e112775db721c2a206f
Sina Sartip <ssartipz@ryerson.ca>
2021-04-04 22:21:30 -0400

    read csv fixed

--- model/GrabStockData.py
+++ model/GrabStockData.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import os 
 import shutil
 import numpy as np
+import pandas as pd
 
 
 class dataGrabber():
@@ -38,14 +39,18 @@ class dataGrabber():
     def grab_stockData(self):
         self.StockData = yf.download(self.tickerList,period='1mo',interval='1d',group_by='ticker',threads=True)
 
-    def load_stockData(self):
-        pass
+    def load_stockData(self,SQL=True):
+        #TODO try block for no csv file
+        if SQL:
+            self.SQL_ready_df = pd.read_csv('./model/csv_data/SQLReady_Stock_Data.csv')
+        else:
+            self.StockData = pd.read_csv('./model/csv_data/Stock_Data.csv')    
 
     def save_stockDataRAW(self):
         self.StockData.to_csv('./model/csv_data/Stock_Data.csv')
     
     def save_stockDataTSQL(self):
-        SQL_ready_df = self.StockData.stack(level=0).rename_axis(['Date', 'Ticker']).reset_index(level=1) #fix for multilevel column issue of yfinance
-        SQL_ready_df = SQL_ready_df.round({"Adj Close":4, "Close":4, "High":4, "Low":4, "Open":4})
-        SQL_ready_df.to_csv('./model/csv_data/SQLReady_Stock_Data.csv')       
+        self.SQL_ready_df = self.StockData.stack(level=0).rename_axis(['Date', 'Ticker']).reset_index(level=1) #fix for multilevel column issue of yfinance
+        self.SQL_ready_df = self.SQL_ready_df.round({"Adj Close":4, "Close":4, "High":4, "Low":4, "Open":4})
+        self.SQL_ready_df.to_csv('./model/csv_data/SQLReady_Stock_Data.csv')       
 
